04988a066a7b38d2ddde42837bce11178befd19c
Stuart Bourhill <stuburger@gmail.com>
2018-02-02 12:09:49 +0200

    fixed bug where validation helpers were not correctly exported

--- index.js
+++ index.js
@@ -1,8 +1,3 @@
 'use strict';
 
-var form =  require('./lib/withForm')
-module.exports = form.default;
-exports.isRequired =  form.isRequired;
-exports.maxLength =  form.maxLength;
-exports.minLength =  form.minLength;
-exports.email =  form.email;
+module.exports = require('./lib/withForm');
